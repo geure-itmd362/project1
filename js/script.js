d47d70c547497e39424a4b504bc87363633d0630
Greg Eure <geure@hawk.iit.edu>
2020-10-15 19:02:10 -0500

    Added thank you button using html, css, javascript using preventDefault()

--- js/script.js
+++ js/script.js
@@ -7,14 +7,47 @@ $(document).ready(function() {
     $("#submit").css("display", "block");
     })
 
-  /* not working
-  $('#submit').click(function() {
+  /* not working */
+  $('#submit').click(function(e) {
+    e.preventDefault();
+    var my_inputs = {
+      name: $("#name").val(),
+      email: $("#email").val(),
+      birthday: $("#birthday").val()
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "#null", 
+      data: my_inputs,
+      success: function(data, status){
+        console.log(data);
+        console.log(status);
+        $('#done').show();
+      },
+      error: function(err){
+        console.log(err);
+      }
+    });
+
+    // for resetting the form
+    $("#name").val("");
+    $("#email").val("");
+    $("#birthday").val("");
+  });
+
+  $('#finish').click(function(){
+    $('#done').hide();
+  });
+
+    /*
     $("#form").toggle();
     $("#container").toggle();
+    alert("I am an alert box!");
     $("#done").toggle();
     $("#finish").toggle();
-  })
-  */
+    })
+    */
 
   $('#name').focus(function() {
     $(this).attr('placeholder', 'enter first and last name...')
