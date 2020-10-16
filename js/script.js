73c79c287ee7db0e18a4cebc514392d5a04c66e6
Greg Eure <geure@hawk.iit.edu>
2020-10-15 22:50:24 -0500

    Removed broken code for css-styled "done" button and added system alert button

--- js/script.js
+++ js/script.js
@@ -7,48 +7,6 @@ $(document).ready(function() {
     $("#submit").css("display", "block");
     })
 
-  /* not working */
-  $('#submit').click(function(e) {
-    e.preventDefault();
-    var my_inputs = {
-      name: $("#name").val(),
-      email: $("#email").val(),
-      birthday: $("#birthday").val()
-    }
-
-    $.ajax({
-      type: "POST",
-      url: "https://ptsv2.com/t/3tp3r-1602814424", 
-      data: my_inputs,
-      success: function(data, status){
-        console.log(data);
-        console.log(status);
-        $('#done').show();
-      },
-      error: function(err){
-        console.log(err);
-      }
-    });
-
-    // for resetting the form
-    $("#name").val("");
-    $("#email").val("");
-    $("#birthday").val("");
-  });
-
-  $('#finish').click(function(){
-    $('#done').hide();
-  });
-
-    /*
-    $("#form").toggle();
-    $("#container").toggle();
-    alert("I am an alert box!");
-    $("#done").toggle();
-    $("#finish").toggle();
-    })
-    */
-
   $('#name').focus(function() {
     $(this).attr('placeholder', 'enter first and last name...')
     }).blur(function() {
@@ -72,4 +30,9 @@ $(document).ready(function() {
     }).blur(function() {
       $(this).attr('placeholder', 'Birthday')
     })
+
+  $('#submit').click(function() { 
+    alert("Congratulations! Spectacular photos coming your way!");
+  })
+
 });
